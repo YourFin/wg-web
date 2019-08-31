9f886388ada0a2ee9665d9ad6c713fbaf6a82f86
YourFin <d.junkpobox+git@gmail.com>
2019-08-30 17:07:43 -0700

    Add endpoint record

---
+++ db/migrate/20190831000609_create_endpoints.rb
@@ -0,0 +1,10 @@
+class CreateEndpoints < ActiveRecord::Migration[5.2]
+  def change
+    create_table :endpoints do |t|
+      t.string :name
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
