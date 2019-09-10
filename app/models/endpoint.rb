aa84e6c02742a5b268adb70e65943420cc22d5b1
YourFin <d.junkpobox+git@gmail.com>
2019-09-09 20:23:37 -0700

    add endpoints and peers

--- app/models/endpoint.rb
+++ app/models/endpoint.rb
@@ -1,2 +1,4 @@
 class Endpoint < ApplicationRecord
+  has_many :peer
+  validates :name, presence: true
 end
--- app/models/peer.rb
+++ app/models/peer.rb
@@ -1,2 +1,5 @@
 class Peer < ApplicationRecord
+  has_one :endpoint
+  validates :name, presence: true
+  validates :public_key, presence: true
 end
